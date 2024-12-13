a923e8a2684f4cecb585960e44ace5474181eb37
Sebastian Pardo <sebpardo@gmail.com>
2024-12-13 14:54:53 -0800

    Update to Dec 2024 file

--- R/seb_key.R
+++ R/seb_key.R
@@ -4,7 +4,7 @@ library(tidyverse)
 
 
 stock_key <- readRDS(
-  here::here("data", "generated", "finalStockList_Nov2024.rds")
+  here::here("data", "generated", "finalStockList_Dec2024.rds")
   )
 
 seb_key <- read.csv(
